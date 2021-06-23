03cbb9f609a4f918490a162ed83001726669ad8d
Jorge Rodrigo Torrez Aramayo <jorgerodrigotorrez@gmail.com>
2021-06-23 16:00:58 -0400

    Value type reference practice uploaded

---
+++ ValueReferenceType/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ValueReferenceType
+{
+    class Program
+    {
+        class DetailedInteger
+        {
+            public int Number;
+            List<string> Letters;
+
+            public DetailedInteger(int newNumber)
+            {
+                Number = newNumber;
+                Letters = new();
+            }
+
+            public DetailedInteger(DetailedInteger tech)
+            {
+                Number = tech.Number;
+                Letters = new();
+            }
+
+            public void AddDetail(string newString)
+            {
+                this.Letters.Add(newString);
+            }
+
+            public override string ToString()
+            {
+                return $"{Number} [{string.Join(",", Letters.ToArray())}]";
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var n1 = new DetailedInteger(0);
+            n1.AddDetail("A");
+            Console.WriteLine(n1);
+
+            var n2 = n1;
+            n2.Number = 7;
+            n2.AddDetail("B");
+
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+
+        }
+    }
+}
