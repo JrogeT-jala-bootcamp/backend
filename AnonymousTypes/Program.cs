dbcf860d04c9cf9bc90a364289092cc6633214ce
Jorge Rodrigo Torrez Aramayo <jorgerodrigotorrez@gmail.com>
2021-06-29 15:55:50 -0400

    Anonymous type class uploaded

---
+++ AnonymousTypes/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace AnonymousTypes
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Anonymous anonymous = new Anonymous();
+
+            dynamic anonymousDynamicData = anonymous.getData();
+            Console.WriteLine(
+                string.Format("{0} {1}",
+                anonymousDynamicData.Name,
+                anonymousDynamicData.EmailID)
+            );
+
+            object obj = anonymous.getData();
+            Console.WriteLine(
+                string.Format("{0} {1}",
+                    ((dynamic)obj).Name,
+                    ((dynamic)obj).EmailID)
+            );
+        }
+        public object getData()
+        {
+            return new { Name = "Pepe", EmailID = "pepe@gmail.com" };
+        }
+
+        class Anonymous
+        {
+            public object getData()
+            {
+                return new { Name = "Pepe", EmailID = "pepe@gmail.com" };
+            }
+        }
+
+    }
+
+}
